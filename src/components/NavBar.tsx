590d87200fb4f12ebc8bab886c8d4155e072c448
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-09-08 10:53:12 +0300

    End minimum

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './NavBar.module.css'
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {AUTH_ROUTE, MAIN_ROUTE} from "../utils/consts";
 import {AppDispatch, RootState} from "../store/store";
 import {useDispatch, useSelector} from "react-redux";
@@ -11,19 +11,31 @@ const NavBar = () => {
     const dispatch: AppDispatch = useDispatch();
     const isAuth = useSelector((state: RootState) => state.userReducer.isAuth);
     const user = useSelector((state: RootState) => state.userReducer.user);
+    const navigate = useNavigate()
 
     const handleLogOut = () => {
-        dispatch(logOutAction());
+        dispatch(logOutAction())
+        navigate(AUTH_ROUTE);
     };
 
+    const handleNavigateMain = () => {
+        navigate(MAIN_ROUTE);
+    };
+
+    const handleNavigateAuth = () => {
+        navigate(AUTH_ROUTE);
+    };
 
     return (
         <div className={style.navbar}>
             {isAuth && user ? (
                 <>
-                    <NavLink to={MAIN_ROUTE} className={style.link}>
-                        <h3 className={style.label}>Best Application</h3>
-                    </NavLink>
+                    <h3
+                        className={style.label}
+                        onClick={handleNavigateMain}
+                    >
+                        Best Application
+                    </h3>
                     <>
                         <h2 className={style.welcome_message}>Welcome, {user.username}</h2>
                         <div>name: {user.name}</div>
@@ -38,12 +50,18 @@ const NavBar = () => {
                 </>
             ) : (
                 <>
-                    <NavLink to={MAIN_ROUTE} className={style.link}>
-                        <h3 className={style.label}>Best Application</h3>
-                    </NavLink>
-                    <NavLink to={AUTH_ROUTE}>
-                        <button className={style.button}>Sign In</button>
-                    </NavLink>
+                    <h3
+                        className={style.label}
+                        onClick={handleNavigateMain}
+                    >
+                        Best Application
+                    </h3>
+                    <button
+                        className={style.button}
+                        onClick={handleNavigateAuth}
+                    >
+                        Sign In
+                    </button>
                 </>
             )}
         </div>
